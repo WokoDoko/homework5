3c729a7a2f8fe0310c65688096f6f25a1d9de0e4
WokoDoko <76409496+WokoDoko@users.noreply.github.com>
2021-01-13 15:48:43 +0300

    Add files via upload

---
+++ homework5/src/geekbrains/fifthVebinar/Employee.java
@@ -0,0 +1,32 @@
+package geekbrains.fifthVebinar;
+
+public class Employee {
+
+    private final String fullName;
+    private final String position;
+    private final String email;
+    private final String phoneNum;
+    private final int wage;
+    private final byte age;
+
+    public Employee (String fullName, String position,String email,String phoneNum,int wage,byte age){
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.phoneNum = phoneNum;
+        this.wage = wage;
+        this.age = age;
+        printEmployeeInfo();
+
+    }
+
+    public void printEmployeeInfo () {
+        int arrayCellCount = 1;
+        while (age > 40 && arrayCellCount > 0) {
+            System.out.println("ФИО: " + fullName + "\n" + "Должность: " + position + "\n" + "email: " + email + "\n" + "Телефонный номер: " + phoneNum + "\n" + "Заработная плата: " + wage + "\n" + "Возраст: " + age);
+            System.out.println();
+            arrayCellCount--;
+        }
+    }
+
+}
